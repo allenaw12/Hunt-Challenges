9dc019c39be31ff3e70c7041bc575498e97ee3cc
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-10-19 13:58:25 -0500

    added leon's challenge question to js

--- main2.js
+++ main2.js
@@ -15,3 +15,11 @@ function sumMix(arr){
 console.log(sumMix([9,3,'7','3']), 22)
 console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42)
 console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41)
+
+//Day four(posted Oct 19, due Oct 19)
+//It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+// getAverage([2,2,2,2]),2)
+// getAverage([1,2,3,4,5,]),3);
+// getAverage([1,1,1,1,1,1,1,2]),1)
