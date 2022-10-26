ae021e51f017c2d8afd2b195ef461dec9dce7868
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-10-26 13:20:24 -0500

    added leon's challenge

--- main3.js
+++ main3.js
@@ -23,4 +23,12 @@ console.log(uniqueArray([[1,2],[3,4],[5,6]]),"8")
 console.log(uniqueArray([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),"72")
 
     //Day 8 (posted Oct 26, due Oct 27)
-//
+//Given an array of integers , Find the Nth smallest element in this array of integers.
+// Notes
+// Array/list size is at least 3 .
+// Array/list's numbers could be a mixture of positives , negatives and zeros .
+// Repetition in array/list's numbers could occur , so don't Remove Duplications .
+// nthSmallest({3,1,2} ,2) ==> return (2) 
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+// nthSmallest({15,20,7,10,4,3} ,3) ==> return (7) 
+// nthSmallest({177,225,243,-169,-12,-5,2,92} ,5) ==> return (92)
