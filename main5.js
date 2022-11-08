e5b1ba83b2f15637b4edcc135d25ccd6d069aeea
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-11-08 13:24:21 -0600

    get ready for future challenge

--- main5.js
+++ main5.js
@@ -22,3 +22,6 @@ function findDeletedNumber(arr1, arr2){
 console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]), "2")
 console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), "5")
 console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), "0")
+
+    //Day 13(posted Nov)
+//
