fa3408fc293be78c475b82b310e802ac7506909a
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-11-10 12:31:29 -0600

    get ready for the challenge question

--- main5.js
+++ main5.js
@@ -52,3 +52,6 @@ console.log(arrayDiff([1,2,2,2,3],[2]), "[1,3]")
 console.log(arrayDiff([], [4,5]), "[]")
 console.log(arrayDiff([3,4], [3]), "[4]")
 console.log(arrayDiff([1,8,2], []), "[1,8,2]")
+
+    //Day 14(posted, due)
+//
