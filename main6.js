a58578d33691870c60d7c0c8c93552b73e57153b
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-11-16 17:33:43 -0600

    added leon question

--- main6.js
+++ main6.js
@@ -68,4 +68,10 @@ console.log(sortArray([9,8,7,6,5,4,3,2,1,0]), "1,8,3,6,5,4,7,2,9,0")
 console.log(sortArray([5,3,1,8,0]), "1,3,5,8,0")
 
     //Day 16 (Posted Nov 16, due nov 17)
-//
+//Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+// A few examples:
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+// If the average length is not an integer, use Math.round().
+// The input array's length > 1
