e4a4063add806cd8b9de3b101b98cdf6ad7eb0a0
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-11-20 17:50:11 -0600

    added leon question

--- main7.js
+++ main7.js
@@ -1,2 +1,4 @@
-    //Day 17(posted Nov, due Nov)
-//
+    //Day 17(posted Nov 21, due Nov 21)
+//Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+// --> "alpha beta gamma delta alpha beta gamma delta"
