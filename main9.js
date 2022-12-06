9dcb5b654d9abb649d5e2960069eabd555e8b1c0
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-12-06 16:04:54 -0600

    added question and prep and first run of solution

--- main9.js
+++ main9.js
@@ -32,4 +32,25 @@ console.log(countCharacters('aba'),"{'a': 2, 'b': 1}")
 console.log(countCharacters('hello'),"{'h': 1, 'e': 1, 'l': 2, 'o': 1}")
 
     //day 23
-//
+//You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+// Example 1:
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+// Example 2:
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+// Constraints:
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
+
+//parameters take in 2 strings, jewels and stones, jewels will always have a length 1 or greater, stones will have a length of 50 or less, jewels' characters will be unique and both strings only consist of english letter. case sensitive, different case of same letter is  different value. can there be other value types? 
+function jewelsAndStones(jewels, stones){
+    //first thought is filter, if stones includes jewels, filter it, then return length of that new arr
+    return stones.split('').filter(char=>jewels.includes(char)).length
+}
+//returns an integer number of the count of stones that match jewels
+console.log(jewelsAndStones("z","ZZ"),'0')
+console.log(jewelsAndStones("aA","aAAbbbb"),'3')
+console.log(jewelsAndStones("bBd","AbrcADaBra"),'2')
