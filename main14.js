395f0b2036929007dc1bdfde6bf0bfae028436e9
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-02-01 16:53:33 -0600

    updated title tag in index, rewrote euclidean algorithm for today's practice huntober

--- main14.js
+++ main14.js
@@ -37,15 +37,34 @@ let mygcd = (int1, int2) => {
 
     //recursion you say....
         //first attempt
-    let max=Math.max(int1, int2)
-    let min=Math.min(int1, int2)
-    console.log(max, min)
-    if(max % min === 0){
-        //console.log(min)
-        return min
+    // let max=Math.max(int1, int2)
+    // let min=Math.min(int1, int2)
+    // console.log(max, min)
+    // if(max % min === 0){
+    //     //console.log(min)
+    //     return min
+    // }else{
+    //     console.log(max%min)
+    //     return mygcd(max, max%min)
+    // }
+
+    //leon's quick not recursion
+    // for(let i = Math.min(int1,int2); i > 0; i--){
+    //     if( int1 % i === 0 && int2 % i === 0  ){
+    //       return i
+    //     }
+    //   }
+
+    //euclidean algorithm
+    //larger num = smaller num * (number of times it can go into bigger num) + (remainder)
+    if(int1===0){
+        return int2
+    }else if(int2===0){
+        return int1
     }else{
-        console.log(max%min)
-        return mygcd(max, max%min)
+        let order = [Math.max(int1, int2), Math.min(int1, int2)]
+        let r = order[0]%order[1]
+        return mygcd(order[1], r)
     }
 }
 //returns the highest common divisor of the 2 given numbers, at least 1
@@ -55,3 +74,24 @@ console.log(mygcd(1,1),1)
 console.log(mygcd(60,12),12)
 console.log(mygcd(10927782,6902514), 846)
 console.log(mygcd(1590771464,1590771620),4)
+
+    //Day 35
+//recursion with algorithm
+let mgcdRecurs = (num1, num2) => {
+    if(num1===0){
+        return num2
+    }else if(num2===0){
+        return num1
+    }else{
+        let max = Math.max(num1, num2)
+        let min = Math.min(num1, num2)
+        let rem = max % min
+        return mgcdRecurs(min, rem)
+    }
+}
+console.log(mgcdRecurs(30,12),6)
+console.log(mgcdRecurs(8,9),1)
+console.log(mgcdRecurs(1,1),1)
+console.log(mgcdRecurs(60,12),12)
+console.log(mgcdRecurs(10927782,6902514), 846)
+console.log(mgcdRecurs(1590771464,1590771620),4)
