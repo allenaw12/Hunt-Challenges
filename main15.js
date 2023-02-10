e3c8a57e907ab2fc973e0789309be936b231f3a8
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-02-10 14:14:10 -0600

    linked list practice from yesterday

--- main15.js
+++ main15.js
@@ -67,16 +67,14 @@ class Stack{
         this.size = 0
     }
     push(value){
-        this.stack[this.size] = value
         this.size++
-        console.log(this.stack, this.size)
+        this.stack[this.size] = value
     }
     pop(){
         if(this.size === 0) return 'nothing to remove'
-        this.size--
         let removed = this.stack[this.size]
         delete this.stack[this.size]
-        console.log(this.stack, this.size, removed)
+        this.size--
         return removed
     }
     peek(){
@@ -90,3 +88,90 @@ stack.push('jurassic park')
 stack.push('fountainhead')
 stack.push('jumanji')
 stack.pop()
+
+    //Day 38
+//Linked lists!
+//can have singly and doubly linked lists, also circular type! (where first and last refer to each other)
+//array vs linked list: array in memory is all in order in a continuous chunk of memory, linked list in mem can be anywhere but their connected to each other
+//doubly linkedlist
+class LinkedList {
+    constructor(){
+        this.head = null
+        this.tail = null
+    }
+    //add to end
+    append(value){
+        //if list empty
+        if(!this.tail){
+            this.head = this.tail = new Node(value)
+        }else{
+            let oldTail = this.tail
+            this.tail = new Node(value)
+            oldTail.next = this.tail
+            this.tail.prev = oldTail
+        }
+    }
+    //add to beginning
+    prepend(value){
+        //if list empty
+        if(!this.head){
+            this.head = this.tail = new Node(value)
+        }else{
+            let oldHead = this.head
+            this.head = new Node(value)
+            oldHead.prev = this.head
+            this.head.next = oldHead
+        }
+    }
+    deleteHead(){
+        //if list empty
+        if(!this.head){
+            return null
+        }else{
+            let removedHead = this.head
+            //if 1 node left
+            if(this.head === this.tail){
+                this.head = this.tail = null
+            }else{
+                this.head = this.head.next
+                this.head.prev = null
+            }
+            return removedHead.value
+        }
+    }
+    deleteTail(){
+        //if list empty
+        if(!this.tail){
+            return null
+        }else{
+            let removedTail = this.tail
+            //if 1 node left
+            if(this.head === this.tail){
+                this.head = this.tail = null
+            }else{
+                this.tail = this.tail.prev
+                this.tail.next = null
+            }
+            return removedTail.value
+        }
+
+    }
+    search(value){
+        let currentNode = this.head
+        while(currentNode){
+            if(currentNode.value === value){
+                return currentNode
+            }
+            currentNode = currentNode.next
+        }
+        return null
+    }
+}
+
+class Node {
+    constructor(value, prev, next){
+        this.value = value
+        this.prev = prev || null
+        this.next = next || null
+    }
+}
