35692b9c7fd47a3fffe009999452b29d0e712c67
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-02-13 18:01:12 -0600

    linked list practice

--- main16.js
+++ main16.js
@@ -51,7 +51,7 @@ class Stack {
 //queue class object
 class Queue{
     constructor(){
-        let storage = {}
+        this.storage = {}
         this.head = 0
         this.tail = 0
     }
@@ -67,5 +67,82 @@ class Queue{
     }
     size(){
         return this.tail - this.head
+    }
+}
+//linked list
+class LinkedList{
+    constructor(){
+        this.head = this.tail = null
+    }
+    append(value){
+        //if empty list
+        if(!this.tail){
+            this.head =this.tail=new Node(value)
+        }else{
+            let oldTail = this.tail
+            this.tail = new Node(value)
+            oldTail.next = this.tail
+            this.tail.prev = oldTail
+        }
+    }
+    prepend(value){
+        //if empty list
+        if(!this.head){
+            this.head = this.tail = new Node(value)
+        }else{
+            let oldHead = this.head
+            this.head = new Node(value)
+            oldHead.prev = this.head
+            this.head.next = oldHead
+        }
+
+    }
+    removeHead(){
+        //if empty list
+        if(!this.head){
+            return null
+        }else{
+            let removed =  this.head
+            //if last item
+            if(this.head === this.tail){
+                this.head = this.tail = null
+            }else{
+                this.head = this.head.next
+                this.head.prev =  null
+            }
+            return removed.value
+        }
+    }
+    removeTail(){
+        if(!this.tail){
+            return null
+        }else{
+            let removed =  this.tail
+            //if last node
+            if(this.head === this.tail){
+                this.head =  this.tail = null
+            }else{
+                this.tail.prev =  this.tail
+                this.tail.next = null
+            }
+            return removed.value
+        }
+    }
+    search(value){
+        let currentNode = this.head
+        while(currentNode){
+            if(currentNode.value === value){
+                return currentNode
+            }
+            currentNode = currentNode.next
+        }
+        return null
+    }
+}
+class Node{
+    constructor(value, prev, next){
+        this.value = value
+        this.prev = prev || null
+        this.next = next || null
     }
 }
