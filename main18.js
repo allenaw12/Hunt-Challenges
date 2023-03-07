59a56eaedec823e3d739f5a84f33893f3941532a
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-03-07 17:15:33 -0600

    notes on trees

---
+++ main18.js
@@ -0,0 +1,20 @@
+    //Day 41
+//high level for trees
+//linked list => made up of nodes => each node has a value and pointer(next)
+
+//NODE BASED STRUCTURES
+//graphs=>nodes that can all be connected to each other
+//eg: cities=>package from sf to atl taking different routes
+    //cycles...
+
+//trees=>heirarchal orginate and branch out (looks like upside down tree)
+//og node is called 'root' node
+//parent child relationships, parent child nodes, one to many relationship
+//different kinds of trees
+    //eg: heaps=> trees but keep stuff in order
+    //eg: tries=> keeping words in order
+    //eg: binary tree=> each node has max of 2 children/pointers
+//linked list is like one dimensional tree
+//binary trees help with => search algorithms, sorting algorithms, file systems
+
+//DOM is a tree node structure! :O
